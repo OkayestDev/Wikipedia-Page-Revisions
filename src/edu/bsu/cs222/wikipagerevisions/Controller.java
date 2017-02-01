86342f64ee24289a29849f7292797b756c1138d0
Sir-Kyle-Richardson <krichardson5@bsu.edu>
2017-02-01 16:29:57 -0500

    First commit, base for TWP

--- src/edu/bsu/cs222/wikipagerevisions/Controller.java
+++ src/edu/bsu/cs222/wikipagerevisions/Controller.java
@@ -1,8 +1,17 @@
 package edu.bsu.cs222.wikipagerevisions;
 
-public class Controller {
-    public void getXMLFile()
-    {
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
 
+public class Controller {
+    private String authors[][] = new String[10][3]; //hold revision authors [][username, timestamp, comment]
+
+    @FXML
+    private TextField searchFeild;
+
+    public void openXMLFile()
+    {
+    
     }
 }
