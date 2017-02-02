8b4f0b3dd267ba918579189b25021607825e0565
Sir-Kyle-Richardson <krichardson5@bsu.edu>
2017-02-01 20:10:19 -0500

    Working on opening XML file

--- src/edu/bsu/cs222/wikipagerevisions/Controller.java
+++ src/edu/bsu/cs222/wikipagerevisions/Controller.java
@@ -1,17 +1,39 @@
 package edu.bsu.cs222.wikipagerevisions;
 
 import javafx.fxml.FXML;
-import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
+import java.io.IOException;
+import java.net.URL;
 
 public class Controller {
-    private String authors[][] = new String[10][3]; //hold revision authors [][username, timestamp, comment]
 
     @FXML
-    private TextField searchFeild;
+    private TextField searchField;
 
-    public void openXMLFile()
+    private String authors[][] = new String[10][3]; //hold revision authors [][username, timestamp, comment]
+    private int count;
+
+    @FXML
+    public void openXML()
     {
+        String urlStart = "https://en.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&titles=";
+        String urlEnd = "&rvprop=timestamp|comment|user&rvlimit=4;";
+        String search = searchField.getText();
+        search = search.replace(" ", "_");
+        search = urlStart + search + urlEnd;
 
+        try {
+            URL url = new URL(search);
+            //System.out.println(url);
+        }
+        catch(IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void readXMLFile()
+    {
+        count = 0;
     }
 }
---
+++ test/edu/bsu/cs222/wikipagerevisionstests/revisionTests.java
@@ -0,0 +1,11 @@
+package edu.bsu.cs222.wikipagerevisionstests;
+
+import org.junit.Test;
+
+public class revisionTests {
+    @Test
+    public void testOpenXML()
+    {
+
+    }
+}
