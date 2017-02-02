67537df661919ea705f24a18e4a382bfcb35d443
Sir-Kyle-Richardson <krichardson5@bsu.edu>
2017-02-01 21:12:52 -0500

    Working on opening XML file

--- src/edu/bsu/cs222/wikipagerevisions/Controller.java
+++ src/edu/bsu/cs222/wikipagerevisions/Controller.java
@@ -2,38 +2,48 @@ package edu.bsu.cs222.wikipagerevisions;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class Controller {
-
     @FXML
     private TextField searchField;
-
-    private String authors[][] = new String[10][3]; //hold revision authors [][username, timestamp, comment]
-    private int count;
+    private String revisions[][] = new String[10][3]; //hold revision authors [][username, timestamp, comment]
 
     @FXML
-    public void openXML()
-    {
-        String urlStart = "https://en.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&titles=";
-        String urlEnd = "&rvprop=timestamp|comment|user&rvlimit=4;";
+    public void loadURL() {
+        String URLStart = "https://en.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&titles=";
+        String URLEnd = "&rvprop=timestamp|comment|user&rvlimit=4";
         String search = searchField.getText();
         search = search.replace(" ", "_");
-        search = urlStart + search + urlEnd;
-
+        search = URLStart + search + URLEnd;
         try {
             URL url = new URL(search);
-            //System.out.println(url);
+            System.out.println(url);
+            readXMLFile(url);
         }
-        catch(IOException e)
-        {
+        catch(IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void readXMLFile()
-    {
-        count = 0;
+    public void readXMLFile(URL url) {
+        int count = 0;
+        try {
+            URLConnection connection = url.openConnection();
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(connection.getInputStream());
+
+            System.out.println();
+        }
+        catch(Exception e){
+            throw new RuntimeException(e);
+        }
     }
 }
--- test/edu/bsu/cs222/wikipagerevisionstests/revisionTests.java
+++ test/edu/bsu/cs222/wikipagerevisionstests/revisionTests.java
@@ -1,11 +1,35 @@
 package edu.bsu.cs222.wikipagerevisionstests;
 
+import edu.bsu.cs222.wikipagerevisions.*;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.Assert;
 import org.junit.Test;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
 
 public class revisionTests {
-    @Test
-    public void testOpenXML()
-    {
 
+    @FXML
+    private Controller controllerTest = new Controller();
+
+    @FXML
+    @Test
+    public void testLoadURL() {
+//        controllerTest.searchField.setText("Barrack Obama");
+//        try {
+//            URL url = new URL("https://en.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&titles=Barrack_Obama" +
+//                    "&rvprop=timestamp|comment|user&rvlimit=4");
+//            Assert.assertEquals(url, controllerTest.loadURL());
+//        }
+//        catch(IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
