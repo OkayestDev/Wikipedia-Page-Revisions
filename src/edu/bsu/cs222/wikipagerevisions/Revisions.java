7972400cb3399dbd8588b9b89efac7d7867e6abe
Sir-Kyle-Richardson <khooblahh@gmail.com>
2017-02-03 08:39:24 -0500

    Working on parsing XML file and handling

--- src/edu/bsu/cs222/wikipagerevisions/Revisions.java
+++ src/edu/bsu/cs222/wikipagerevisions/Revisions.java
@@ -7,15 +7,5 @@ public class Revisions {
     String comment;
     String user;
     String timestamp; //localize to my timezone
-    List<Revisions> revisionList = new ArrayList<Revisions>();
 
-    public void add(user, comment, timestamp)
-    {
-
-    }
-
-    String getRevisionList()
-    {
-        return revisionList;
-    }
 }
