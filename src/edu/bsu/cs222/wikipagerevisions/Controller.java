0d2b72fb22ee2ddf26b8a0a571bdb050ed1ab3ca
Sir-Kyle-Richardson <khooblahh@gmail.com>
2017-02-06 10:15:34 -0500

    Working towards MCV design, still need to load revisions to GUI

--- src/edu/bsu/cs222/wikipagerevisions/Controller.java
+++ src/edu/bsu/cs222/wikipagerevisions/Controller.java
@@ -1,5 +1,6 @@
 package edu.bsu.cs222.wikipagerevisions;
 
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TableColumn;
@@ -23,104 +24,30 @@ public class Controller {
     private TableColumn userColumn;
     @FXML
     private TableColumn timestampColumn;
+    private Model model = new Model();
+    private List<Revisions> revisionsList = new ArrayList<>();
 
-    public URL handleButtonPress()
-    {
-        return loadURL(searchField.getText());
-    }
-
-    public URL loadURL(String search) {
-        String URLStart = "https://en.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&titles=";
-        String URLEnd = "&rvprop=timestamp|comment|user&rvlimit=30&redirects";
-        search = search.replace(" ", "_");
-        search = URLStart + search + URLEnd;
-        try {
-            URLtoDoc(new URL(search)); //this is probably not how to do it
-            return new URL(search);
-        }
-        catch(IOException e) { //Handle exceptions better
-            throw new RuntimeException(e);
-        }
-    }
-
-    public Document URLtoDoc(URL url)
-    {
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(url.openStream());
-            loadRevisionsToGUI(doc); //this is probably not how to do it
-            return doc;
-        }
-        catch(SAXException e) {
-            System.out.println("Check internet connection or URL does not exist");
-        }
-        catch(IOException e) {
-            System.out.println("A problem occured when retrieving URL");
-        }
-        catch(ParserConfigurationException e){
-            System.out.println("URL is not in XML format");
-        }
-        return null;
-    }
-
-    public List<Revisions> parseRevisions(Document doc) {
-        List<Revisions> revisionsList = new ArrayList<>();
-        try {
-            if (doesPageExist(doc)) {
-                if (doesPageHaveRevisions(doc)) {
-                    NodeList numberOfRevisions = doc.getElementsByTagName("rev");
-                    for (int i = 0; i < numberOfRevisions.getLength(); i++) {
-                        Element tempElement = (Element) numberOfRevisions.item(i);
-                        String user = tempElement.getAttribute("user");
-                        String timestamp = tempElement.getAttribute("timestamp");
-                        String comment = tempElement.getAttribute("comment");
-                        Revisions rev = new Revisions();
-                        rev.setInformation(user, timestamp, comment);
-                        revisionsList.add(rev);
-                    }
-                }
+    public void handleButtonPress() {
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                URL url = model.loadURL(searchField.getText());
+                Document doc = model.URLtoDoc(url);
+                revisionsList = model.parseRevisions(doc);
+                loadRevisionsToGUI();
+                return null;
             }
-        }
-        catch(Exception e) {
-            throw new RuntimeException(e);
-        }
-        return revisionsList;
+        };
     }
 
-    public boolean doesPageExist(Document doc)
+    public void loadRevisionsToGUI()
     {
-        NodeList check = doc.getElementsByTagName("page");
-        Element idx = (Element) check.item(0);
-        return !(idx.getAttribute("_idx").equals("-1"));
-    }
-
-    public boolean doesPageHaveRevisions(Document doc)
-    {
-        NodeList check = doc.getElementsByTagName("rev");
-        return check.getLength() >= 1;
-    }
-
-    public boolean isRedirection(Document doc)
-    {
-        NodeList check = doc.getElementsByTagName("redirects");
-        return check.getLength() >= 1;
-    }
-
-    public String getRedirection(Document doc) {
-        NodeList redirection = doc.getElementsByTagName("r");
-        Element redirect = (Element) redirection.item(0);
-        return redirect.getAttribute("to");
-    }
-
-    public void loadRevisionsToGUI(Document doc)
-    {
-        Iterator<Revisions> iter = parseRevisions(doc).iterator();
+        Iterator<Revisions> iter = revisionsList.iterator();
         while(iter.hasNext())
         {
             Revisions rev = iter.next();
-//            userColumn.setText(rev.getUser());
-//            timestampColumn.setText(rev.getTimestamp());
+            userColumn.setText(rev.getUser());
+            timestampColumn.setText(rev.getTimestamp());
         }
     }
 }
---
+++ src/edu/bsu/cs222/wikipagerevisions/Model.java
@@ -0,0 +1,100 @@
+package edu.bsu.cs222.wikipagerevisions;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+    public URL loadURL(String search) {
+        String URLStart = "https://en.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&titles=";
+        String URLEnd = "&rvprop=timestamp|comment|user&rvlimit=30&redirects";
+        search = search.replace(" ", "_");
+        search = URLStart + search + URLEnd;
+        try {
+            return new URL(search);
+        }
+        catch(IOException e) { //Handle exceptions better
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Document URLtoDoc(URL url)
+    {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(url.openStream());
+            return doc;
+        }
+        catch(SAXException e) {
+            System.out.println("Check internet connection or URL does not exist");
+        }
+        catch(IOException e) {
+            System.out.println("A problem occured when retrieving URL");
+        }
+        catch(ParserConfigurationException e){
+            System.out.println("URL is not in XML format");
+        }
+        return null;
+    }
+
+    public List<Revisions> parseRevisions(Document doc) {
+        List<Revisions> revisionsList = new ArrayList<>();
+        try {
+            if (doesPageExist(doc)) {
+                if (doesPageHaveRevisions(doc)) {
+                    NodeList numberOfRevisions = doc.getElementsByTagName("rev");
+                    for (int i = 0; i < numberOfRevisions.getLength(); i++) {
+                        Element tempElement = (Element) numberOfRevisions.item(i);
+                        String user = tempElement.getAttribute("user");
+                        String timestamp = tempElement.getAttribute("timestamp");
+                        String comment = tempElement.getAttribute("comment");
+                        Revisions rev = new Revisions();
+                        rev.setInformation(user, timestamp, comment);
+                        revisionsList.add(rev);
+                    }
+                }
+            }
+        }
+        catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+        return revisionsList;
+    }
+
+    public boolean doesPageExist(Document doc)
+    {
+        NodeList check = doc.getElementsByTagName("page");
+        Element idx = (Element) check.item(0);
+        return !(idx.getAttribute("_idx").equals("-1"));
+    }
+
+    public boolean doesPageHaveRevisions(Document doc)
+    {
+        NodeList check = doc.getElementsByTagName("rev");
+        return check.getLength() >= 1;
+    }
+
+    public boolean isRedirection(Document doc)
+    {
+        NodeList check = doc.getElementsByTagName("redirects");
+        return check.getLength() >= 1;
+    }
+
+    public String getRedirection(Document doc) {
+        NodeList redirection = doc.getElementsByTagName("r");
+        Element redirect = (Element) redirection.item(0);
+        return redirect.getAttribute("to");
+    }
+}
