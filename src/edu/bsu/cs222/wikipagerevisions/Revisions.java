6bfc5e03dff7139938b9d2806ebf6d5545ccefd4
Tslantz <tslantz23@gmail.com>
2017-02-13 00:55:53 -0500

    Add files via upload
    changed it to EST and into 12 hour format

--- src/edu/bsu/cs222/wikipagerevisions/Revisions.java
+++ src/edu/bsu/cs222/wikipagerevisions/Revisions.java
@@ -1,18 +1,59 @@
 package edu.bsu.cs222.wikipagerevisions;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
 public class Revisions {
     private String user;
     private String timestamp;
-    private int revisionsCount;
+    int revisionsCount;
 
     public Revisions(String user, String timestamp) {
-        localizeTimestamp localize = new localizeTimestamp();
         this.user = user;
         revisionsCount = 1;
         if (!timestamp.equals("")) {
-            this.timestamp = localize.reformatTimestamp(timestamp);
-            this.timestamp = localize.localizeTime(this.timestamp);
+            this.timestamp = reformatTimestamp(timestamp);
         }
+    }
+
+    private String localizeTimestamp(String timestamp) {
+        try {
+            TimeZone local = TimeZone.getDefault();
+            TimeZone.setDefault(TimeZone.getTimeZone("EST"));
+            SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd\tHH:mm:ss");
+            SimpleDateFormat dateFormatter12hour = new SimpleDateFormat("yyyy-MM-dd\tK:mm:ss a");
+            Date revisionDate = dateFormatter.parse(timestamp);
+            dateFormatter.setTimeZone(local);
+            TimeZone.setDefault(local);
+            return dateFormatter12hour.format(revisionDate);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String reformatTimestamp(String timestamp) {
+        timestamp = timestamp.replace('T', '\t');
+        timestamp = timestamp.substring(0, timestamp.length() - 1);
+        return localizeTimestamp(timestamp);
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public int getRevisionsCount() {
+        return revisionsCount;
     }
 
     @Override
@@ -20,21 +61,7 @@ public class Revisions {
         return "User: " + user + " Timestamp: " + timestamp + " Count: " + revisionsCount;
     }
 
-    void increaseRevisionsCount() {
+    public void increaseRevisionsCount() {
         revisionsCount++;
-    }
-
-    //TableView of the GUI uses all of the methods below. Compiler doesn't recognize it.
-    public int getRevisionsCount() {
-        return revisionsCount;
-    }
-
-    @SuppressWarnings("unused")
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public String getUser() {
-        return user;
     }
 }
